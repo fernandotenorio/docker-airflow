bde4916ab398688a6a54d5976eca15c4088808f4
fernandotenorio <fnd.tenorio@gmail.com>
2020-04-06 15:13:11 -0300

    add more api hooks

--- dags/train.py
+++ dags/train.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python_operator import BranchPythonOperator
 
 from datetime import datetime
 import urllib.request
@@ -53,11 +54,42 @@ def train_model():
 	response = json.loads(response)
 	print(response)
 
+
+def validate_model(**kwargs):
+	url = 'http://flask-nginx/prediction/api/v1.0/validate'
+	req = urllib.request.Request(url)
+	response = urllib.request.urlopen(req)
+	response = response.read().decode('utf-8')
+	response = json.loads(response)
+	print(response)
+	valid = response['status'] == 'ok'
+
+	if valid:
+		return 'train_new_model'
+	else:
+		return 'restore_previous_model'
+
+
+def restore_model():
+	print('Restoring previous model')
+	url = 'http://flask-nginx/prediction/api/v1.0/restore'
+
+
+def set_new_model():
+	print('Setting new model')
+	url = 'http://flask-nginx/prediction/api/v1.0/setnewmodel'
+
  
 with DAG('train_api', description='Python DAG', schedule_interval='*/1 * * * *', start_date=datetime(2020,3,10), catchup=False) as dag:
 	ini_task = DummyOperator(task_id='ini_task', retries=3)
 	get_data_task = PythonOperator(task_id='get_data', python_callable=get_data)
 	proc_data_task = PythonOperator(task_id='proc_data', python_callable=proc_data)
-	train_model_task = PythonOperator(task_id='train_model', python_callable=train_model)
-	end_task = DummyOperator(task_id='end_task')
-	ini_task >> get_data_task >> proc_data_task >> train_model_task >> end_task
+	train_model_task = PythonOperator(task_id='train_new_model', python_callable=train_model)
+	restore_model_task = PythonOperator(task_id='restore_previous_model', python_callable=train_model)
+	set_new_model_task = PythonOperator(task_id='set_new_model', python_callable=set_new_model)
+	end_task = DummyOperator(task_id='end_task', trigger_rule='none_failed')
+	branch = BranchPythonOperator(task_id='validate_new_model', python_callable=validate_model, provide_context=True)
+
+	ini_task >> get_data_task >> proc_data_task >> branch >> [train_model_task, restore_model_task]
+	train_model_task >> set_new_model_task >> end_task
+	restore_model_task >> end_task
